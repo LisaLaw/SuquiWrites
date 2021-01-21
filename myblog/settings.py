1f35af1644d43bb1ee129263dab7207b8b5b051c
LisaLaw <lisacatlaw@gmail.com>
2021-01-21 11:47:27 +0100

    initial commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "@chl72c^zucuq08h6h35a_%dhi!m+jhclhwt6l!s3(7&6m1v7&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
