ec88cb92bfca47c19e1b008fa25315f56a673835
LisaLaw <lisacatlaw@gmail.com>
2021-01-21 12:38:14 +0100

    added homepage

--- suquiwrites/views.py
+++ suquiwrites/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Entry
 
-# Create your views here.
+
+def home(request):
+    entries = Entry.objects.filter(
+        published_date__lte=timezone.now().order_by("published_date")
+    )
+    return render(request, "suquiwrites/home.hmtl", {"entries": entries})
