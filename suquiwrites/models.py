0bcb784941df7a6f64f715180ce14b601cd84c1c
LisaLaw <lisacatlaw@gmail.com>
2021-01-28 22:37:52 +0100

    revert

--- suquiwrites/models.py
+++ suquiwrites/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from django.conf import settings
-import datetime
-from django.utils import timezone
+from datetime import datetime
 
 
 class Entry(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
-        self.published_date = datetime.date.today()
+    def publish(self, value):
+        self.published_date = datetime.strptime(value, "%d-%m-%Y").date()
         self.save()
 
     def __str__(self):
